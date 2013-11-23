0d1f308f0849e60ca488a602aab05ec7ff9cb98a
Irae <irae@irae.pro.br>
2013-11-23 10:05:35 -0800

    Refactored to use socket.io

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var http = require('http');
-var sockjs = require('sockjs');
 var prompt = require('cli-prompt');
-
 var static_server = require('./static/index.js');
-var connections = [];
+var io = require('socket.io').listen(static_server);
 
-var echo = sockjs.createServer({
-    log: function() {}
-});
-echo.on('connection', function(conn) {
-    conn.on('data', function(message) {
-        // console.log(message);
+io.on('connection', function(socket) {
+    // initial event
+    socket.on('newclient', function(message) {
+        // console.log('new client at: ' + message.url);
     });
-    conn.on('close', function () {
-        connections.splice(connections.indexOf(conn), 1);
-    });
-    connections.push(conn);
 });
+io.set('heartbeat interval', 2000);
+io.set('heartbeat timeout', 6000);
+io.set('log level', 1);
 
-var server = http.createServer();
-echo.installHandlers(server, {prefix:'/moblab'});
-server.listen(3582, '0.0.0.0');
-static_server.listen(3581, '0.0.0.0');
+static_server.listen(3582, '0.0.0.0');
 
 function commandPrompt () {
     prompt(' moblab > ', function (command) {
-        connections.forEach(function (conn) {
-            conn.end(command);
-        });
-        setTimeout(commandPrompt, 10);
+        if (command === 'reload') {
+            io.sockets.emit('reload');
+        }
+        var go = 'go ';
+        var http = 'http://';
+        if (command.indexOf(go) === 0) {
+            var url = command.substr(go.length);
+            if (url.indexOf(http) !== 0) {
+                url = http + url;
+            }
+            io.sockets.emit('go', url);
+        }
+        setTimeout(commandPrompt, 500);
     });
 }
 commandPrompt();
--- static/moblab_client.js
+++ static/moblab_client.js
@@ -1,30 +1,25 @@
-'use strict';
 
-function newConnection() {
-    /* global SockJS, mobLabHost */
-    var sock = new SockJS('http://'+mobLabHost+':3582/moblab');
-    sock.onopen = function() {
-        console.log('moblab open');
-        sock.send('moblab client connected from: ' + document.location.href);
-    };
-    sock.onmessage = function(e) {
-        console.log('moblab message', e.data);
-        if(e.data.indexOf('load:') === 0) {
-            var newLoc = e.data.substr('load:'.length);
-            if (newLoc.indexOf('http://')!==0) {
-                newLoc = 'http://' + newLoc;
-            }
-            document.location.href = newLoc;
-        }
-        if(e.data.indexOf('reload:') === 0) {
-            document.location.reload();
-        }
-    };
-    sock.onclose = function() {
-        console.log('moblab close');
-        setTimeout(newConnection, 1000);
-    };
-}
-// start late to avoid infinite loading
-newConnection();
-
+(function moblabStart() {
+    'use strict';
+    /* global io, mobLabHost */
+    if (typeof io === 'undefined') {
+        return setTimeout(moblabStart, 100);
+    }
+    var socket = io.connect('http://'+mobLabHost+':3582', {
+        'connect timeout': 2000
+    });
+    socket.on('connect', function () {
+        console.log('socket connected');
+        socket.emit('newclient', { url: document.location.href });
+    });
+    socket.on('disconnect', function() {
+        console.log('socket disconnected');
+    });
+    socket.on('reload', function() {
+        document.location.reload();
+    });
+    socket.on('go', function(url) {
+        document.location.reload();
+        document.location.href = url;
+    });
+})();
