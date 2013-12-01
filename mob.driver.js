5cf8cee0fe454e460e41eb4b29304018881695c8
Irae <irae@irae.pro.br>
2013-12-01 00:06:21 -0800

    some disconnection handling

--- mob.driver.js
+++ mob.driver.js
@@ -4,18 +4,28 @@
 var static_server = require('./lib/static_index');
 var io = require('socket.io').listen(static_server);
 var async = require('async');
-// io.set('heartbeat interval', 2 * 1000);
-// io.set('heartbeat timeout', 2.1 * 1000);
+io.set('heartbeat interval', 2 * 1000);
+io.set('heartbeat timeout', 2.1 * 1000);
+io.set('close timeout', 2.2 * 1000);
 io.set('log level', 1);
 
 io.on('connection', function(socket) {
     socket.on('browser', function(info) {
         socket.join('browsers');
         console.log('browser', info);
+        socket.on('disconnect', function() {
+            console.log('browser disconnect');
+            socket.leave('browsers');
+        });
     });
     socket.on('browser_broadcast', function(command, args, doneAll) {
         async.map(io.sockets.clients('browsers'), function (browser, doneOne) {
+            var maxTime = setTimeout(function() {
+                doneOne(null, 'error');
+                browser.disconnect();
+            },2 * 1000);
             browser.emit(command, args, function(data) {
+                clearTimeout(maxTime);
                 var results = [null].concat(Array.prototype.slice.call(arguments));
                 doneOne.apply(browser, results);
             });
